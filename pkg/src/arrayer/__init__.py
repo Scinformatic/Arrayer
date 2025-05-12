3b21b9fabce019c1935c7cc729347fa485273480
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-12 15:43:36 +0200

    Update __init__.py

--- pkg/src/arrayer/__init__.py
+++ pkg/src/arrayer/__init__.py
@@ -1,6 +1,6 @@
 """Arrayer: Array-based operations and algorithms."""
 
-from arrayer import matrix
+from arrayer import matrix, pcloud
 from arrayer.pca import pca
 
-__all__ = ["matrix", "pca"]
+__all__ = ["matrix", "pca", "pcloud"]
