8ce0a4b8919430fd72365fa3b03dff89c67075e1
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-16 00:06:54 +0200

    Update pca.py

--- pkg/src/arrayer/pca.py
+++ pkg/src/arrayer/pca.py
@@ -1,5 +1,8 @@
+"""Principal Component Analysis (PCA)."""
+
 from __future__ import annotations
 
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
 from functools import partial
 
@@ -10,17 +13,121 @@ import jax.numpy as jnp
 from arrayer import exception
 
 if TYPE_CHECKING:
-    from typing import Literal
     from numpy.typing import ArrayLike
 
 
-__all__ = ["pca", "pca_single", "pca_batch"]
+__all__ = [
+    "pca",
+    "pca_single",
+    "pca_batch",
+    "PCAOutput",
+]
 
 
-def pca(
-    points: ArrayLike,
-    variance_type: Literal["raw", "ratio", "biased", "unbiased"] = "unbiased"
-) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+@dataclass
+class PCAOutput:
+    """Principal Component Analysis (PCA) output.
+
+    Note that all given shapes here
+    are for the case of 2D input data (i.e., shape `(n_samples, n_features)`).
+    For 3D input data (i.e., shape `(n_batches, n_samples, n_features)`),
+    the batch dimension is added as the first axis.
+
+    Attributes
+    ----------
+    points
+        Transformed points in PCA space.
+        This has the same shape as the input data,
+        but each point is centered and rotated
+        to align with the principal axes.
+    components
+        Principal component matrix.
+        This is a matrix of shape `(n_features, n_features)`,
+        where each row is a principal component,
+        i.e., an eigenvector of the covariance matrix
+        (sorted from largest to smallest variance).
+        This matrix can act as a rotation matrix
+        to align points with the principal axes.
+        For example, to rotate points in an array `a`
+        where the last axis is the feature axis,
+        i.e., any array of shape `(..., n_features)`,
+        use `a @ self.components.T`, or the equivalent `np.matmul(a, self.components.T)`.
+    singular_values
+        Singular values from SVD.
+    translation
+        Translation vector used to center the input data,
+        as a 1D array of shape `(n_features,)`.
+
+    Notes
+    -----
+    - With input data `input_points`, `self.points` is equal to
+      `(input_points + self.translation) @ self.components.T`.
+    - To reduce the dimensionality of the data from `n_features` to `k` (`k < n_features`),
+      you can simply take the first `k` axes of each point in `self.points`, i.e.,
+      `points_reduced = self.points[..., :k]`, which is equivalent to
+      `(input_points + self.translation) @ self.components[:k].T`.
+    """
+    points: jnp.ndarray
+    components: jnp.ndarray
+    singular_values: jnp.ndarray
+    translation: jnp.ndarray
+
+    def __post_init__(self):
+        self._variance_magnitude = None
+        self._variance_ratio = None
+        self._variance_biased = None
+        self._variance_unbiased = None
+        return
+
+    @property
+    def variance_magnitude(self) -> jnp.ndarray:
+        """Raw variance magnitudes (i.e., PCA energies) explained by the principal components.
+
+        These are the squares of the singular values from SVD.
+        This is a 1D array of shape `(n_features,)`.
+        """
+        if self._variance_magnitude is None:
+            self._variance_magnitude = self.singular_values ** 2
+        return self._variance_magnitude
+
+    @property
+    def variance_ratio(self) -> jnp.ndarray:
+        """Variance ratios explained by the principal components.
+
+        These are the raw variance magnitudes `self.variance_magnitude`
+        normalized to sum to 1 for each batch.
+        """
+        if self._variance_ratio is None:
+            self._variance_ratio = self.variance_magnitude / self.variance_magnitude.sum(axis=-1, keepdims=True)
+        return self._variance_ratio
+
+    @property
+    def variance_biased(self) -> jnp.ndarray:
+        """Biased variances explained by the principal components.
+
+        These are the raw variances `self.variance_magnitude`
+        divided by the number of samples `n_samples`.
+        """
+        if self._variance_biased is None:
+            num_samples = self.points.shape[-2]
+            self._variance_biased = self.variance_magnitude / num_samples
+        return self._variance_biased
+
+    @property
+    def variance_unbiased(self) -> jnp.ndarray:
+        """Unbiased variances explained by the principal components.
+
+        These are the raw variances `self.variance_magnitude`
+        divided by `n_samples - 1`, i.e., applying Bessel's correction.
+        These are the same as the eigenvalues of the covariance matrix.
+        """
+        if self._variance_unbiased is None:
+            num_samples = self.points.shape[-2]
+            self._variance_unbiased = self.variance_magnitude / (num_samples - 1)
+        return self._variance_unbiased
+
+
+def pca(points: ArrayLike) -> PCAOutput:
     """Perform Principal Component Analysis (PCA) on a set of points.
 
     Parameters
@@ -28,55 +135,6 @@ def pca(
     points
         Input data of shape `(n_samples, n_features)`
         or `(n_batches, n_samples, n_features)`.
-    variance_type
-        Which explained variance values to return:
-        - "raw": Raw variance magnitudes (i.e., PCA energies).
-          These are the squares of the singular values.
-        - "ratio": Variance ratios.
-          These are the raw variances normalized to sum to 1.
-        - "biased": Biased variance.
-          This is the raw variances divided by `n_samples`.
-        - "unbiased": Unbiased variance.
-          This is the raw variances divided by `n_samples - 1`,
-          i.e., applying Bessel's correction.
-          This is the same as the eigenvalues of the covariance matrix.
-
-    Returns
-    -------
-    A 4-tuple containing
-    (note that the given shapes here are for the case of 2D input data;
-    for 3D input data, the batch dimension is added as the first axis):
-
-    1. Principal component matrix `P`.
-       This is a matrix of shape `(n_features, n_features)`,
-       where each row is a principal component,
-       i.e., an eigenvector of the covariance matrix
-       (sorted from largest to smallest variance).
-       This matrix can act as a rotation matrix
-       to align points with the principal axes.
-       For example, to rotate points in an array `a`
-       where the last axis is the feature axis,
-       i.e., any array of shape `(..., n_features)`,
-       use `a @ P.T`, or the equivalent `np.matmul(a, P.T)`.
-    2. Variance explained by each principal component.
-       This is a 1D array of shape `(n_features,)`
-       containing the variance explained by each principal component.
-       The type of variance is determined by the `variance_type` parameter.
-    3. Translation vector `t`.
-       This is a 1D array of shape `(n_features,)`
-       representing the translation vector used to center the points,
-       i.e., `points_centered = points + t`.
-    4. Transformed points `points_transformed`.
-       This is a 2D array of shape `(n_samples, n_features)`,
-       where each row is a corresponding point from `points` in the PCA space.
-       The points are centered and rotated to align with the principal axes,
-       i.e., `points_transformed = points_centered @ P.T`.
-
-    To reduce the dimensionality of the data,
-    multiply the points with the first `k` principal components.
-    For example, to eliminate the last dimension,
-    you can use `points_reduced = points @ P[:-1].T`,
-    or `points_centered_and_reduced = (points + t) @ P[:-1].T`.
 
     Notes
     -----
@@ -111,21 +169,22 @@ def pca(
             problem=f"At least 2 features are required, but got {points.shape[1]}."
         )
     if points.ndim == 2:
-        return pca_single(points, variance_type)
-    if points.ndim == 3:
-        return pca_batch(points, variance_type)
-    raise exception.InputError(
-        name="points",
-        value=points,
-        problem=f"Points must be a 2D or 3D array, but is {points.ndim}D."
-    )
+        func = pca_single
+    elif points.ndim == 3:
+        func = pca_batch
+    else:
+        raise exception.InputError(
+            name="points",
+            value=points,
+            problem=f"Points must be a 2D or 3D array, but is {points.ndim}D."
+        )
+    return PCAOutput(*func(points))
 
 
-@partial(jax.jit, static_argnames=("variance_type",))
+@jax.jit
 def pca_single(
-    points: jnp.ndarray,
-    variance_type: Literal["raw", "ratio", "biased", "unbiased"] = "unbiased"
-):
+    points: jnp.ndarray
+) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
     """Perform PCA on a single set of points with shape `(n_samples, n_features)`."""
     # Center points
     center = jnp.mean(points, axis=0)
@@ -133,7 +192,7 @@ def pca_single(
     points_centered = points + translation_vector
 
     # SVD decomposition
-    u, s, vt = jnp.linalg.svd(points_centered, full_matrices=False)
+    u, singular_values, vt = jnp.linalg.svd(points_centered, full_matrices=False)
 
     # Flip eigenvectors' signs to enforce deterministic output
     # Ref: https://github.com/scikit-learn/scikit-learn/blob/aa21650bcfbebeb4dd346307931dd1ed14a6f434/sklearn/utils/extmath.py#L895
@@ -148,11 +207,11 @@ def pca_single(
     det_vt = jnp.linalg.det(vt)
     flip_factor = jnp.where(det_vt < 0, -1.0, 1.0)
     # Flip last row of vt and last column of u (if needed)
-    vt = vt.at[-1].multiply(flip_factor)  # flip last principal component
+    components = vt.at[-1].multiply(flip_factor)  # flip last principal component
     u = u.at[:, -1].multiply(flip_factor)  # adjust projected data to match
 
     # Transformed points (projection)
-    points_transformed = u * s  # equal to `points_centered @ vt.T`
+    points_transformed = u * singular_values  # equal to `points_centered @ vt.T`
 
     # Note that the same can be achieved by eigen decomposition of the covariance matrix:
     #     covariance_matrix = np.cov(points_centered, rowvar=False)
@@ -162,20 +221,8 @@ def pca_single(
     #     principal_components = eigenvectors[:, sorted_indices].T
     #     points_transformed = points @ principal_components
 
-    # Calculate explained variance
-    variance = s ** 2
-    variance = jax.lax.switch(
-        {"raw": 0, "ratio": 1, "biased": 2, "unbiased": 3}[variance_type],
-        [
-            lambda v: v,
-            lambda v: v / v.sum(),
-            lambda v: v / points.shape[0],
-            lambda v: v / (points.shape[0] - 1)
-        ],
-        variance
-    )
-    return vt, variance, translation_vector, points_transformed
+    return points_transformed, components, singular_values, translation_vector
 
 
-pca_batch = jax.vmap(pca_single, in_axes=(0, None))
+pca_batch = jax.vmap(pca_single)
 """Perform PCA on a batch of points with shape `(n_batches, n_samples, n_features)`."""
