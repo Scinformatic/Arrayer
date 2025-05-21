4b0dc4e3652b846ce8fd835ee6b64826f38dd9c6
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-21 19:31:45 +0200

    Update typing.py

--- pkg/src/arrayer/typing.py
+++ pkg/src/arrayer/typing.py
@@ -4,7 +4,19 @@ from typing import TypeAlias
 import jax
 import numpy as np
 from beartype import beartype
-from jaxtyping import jaxtyped, Num
+from jaxtyping import jaxtyped, Num, Bool, Float
+
+
+__all__ = [
+    "Num",
+    "Bool",
+    "Float",
+    "typecheck",
+    "atypecheck",
+    "Array",
+    "JAXArray",
+]
+
 
 typecheck = beartype
 atypecheck = partial(jaxtyped, typechecker=beartype)
