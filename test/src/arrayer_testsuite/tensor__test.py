3f0800bf04553b4d6076e73562db95b527a655bb
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-07-04 03:25:19 +0200

    Update tensor__test.py

--- test/src/arrayer_testsuite/tensor__test.py
+++ test/src/arrayer_testsuite/tensor__test.py
@@ -25,3 +25,20 @@ def argin__test(case: dict) -> None:
     expected = jnp.array(case["expected"])
     result = tensor.argin(**case["input"])
     assert jnp.all(result == expected), f"Failed case: {case['case']}"
+
+
+@pytest.mark.parametrize(
+    "case",
+    test_data["indices_sorted_by_value"]["golden"],
+    ids=[test_case["case"] for test_case in test_data["indices_sorted_by_value"]["golden"]]
+)
+def indices_sorted_by_value__test(case: dict) -> None:
+    """Test `arrayer.tensor.indices_sorted_by_value` function against golden file expectations."""
+    result = tensor.indices_sorted_by_value(**case["input"])
+    expected = case["expected"]
+    if expected:
+        expected = jnp.array(expected)
+    else:
+        input_tensor = jnp.array(case["input"]["tensor"])
+        expected = jnp.stack(jnp.unravel_index(jnp.array([]), input_tensor.shape), axis=-1)
+    assert jnp.array_equal(result, expected), f"Failed case: {case['case']}"
