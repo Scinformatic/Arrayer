c1a369b5698ff3bd785a4214fa9a7aa774ead9ec
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-11-08 18:53:27 +0100

    Update __init__.py

--- pkg/src/arrayer/__init__.py
+++ pkg/src/arrayer/__init__.py
@@ -1,6 +1,14 @@
 """Arrayer: Array-based operations and algorithms."""
 
-from arrayer import dtype, kabsch, matrix, pcloud, tensor
+from arrayer import dtype, kabsch, matrix, mesh, pcloud, tensor
 from arrayer.pca import pca
 
-__all__ = ["dtype", "kabsch", "matrix", "pca", "pcloud", "tensor"]
+__all__ = [
+    "dtype",
+    "kabsch",
+    "matrix",
+    "mesh",
+    "pca",
+    "pcloud",
+    "tensor",
+]
